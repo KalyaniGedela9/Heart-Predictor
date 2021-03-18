313619f0b15e30d0072ac84988df1b8420b40f14
kalyani g <80562086+KalyaniGedela9@users.noreply.github.com>
2021-03-18 12:59:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-
+import os
 from flask import Flask,render_template,request
 import jsonify
 import requests
@@ -149,7 +149,8 @@ def predict():
     else:
         return render_template('frontend.html')
 if __name__=="__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0',port=port,debug=True)
         
         
         
